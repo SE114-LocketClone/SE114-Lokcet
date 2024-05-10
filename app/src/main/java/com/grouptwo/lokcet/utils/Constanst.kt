e16e2707df5d8be7d3e763bdf09a82a9b018c905
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-05-11 00:29:05 +0700

    Change distance to suggest friend

--- app/src/main/java/com/grouptwo/lokcet/utils/Constanst.kt
+++ app/src/main/java/com/grouptwo/lokcet/utils/Constanst.kt
@@ -5,6 +5,6 @@ object Constants {
     const val SPLASH_SCREEN_DURATION = 2000L
     const val AVATAR_API_URL = "https://ui-avatars.com/api/?background=7A7878&color=E5A500&name="
     const val EARTH_RADIUS = 6371.0
-    const val MAX_DISTANCE = 10.0
+    const val MAX_DISTANCE = 50.0 // For now we are considering 50 km as max distance (Around Ho Chi Minh City)
     const val PAGE_SIZE = 10L
 }
